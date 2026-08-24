926678c4cfcbd8a599179e1b0499764ba011d1b8
Diego Sanjur <drsgors21@gmail.com>
2026-08-24 11:19:55 -0500

    Programa 1 y Excel de validaciones

---
+++ Pract1-Controles Elaborado por Diego Sanjur/Pract1-Controles Elaborado por Diego Sanjur/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Pract1_Controles_Elaborado_por_Diego_Sanjur
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDia = new System.Windows.Forms.Label();
+            this.lblMes = new System.Windows.Forms.Label();
+            this.lblAño = new System.Windows.Forms.Label();
+            this.txtDia = new System.Windows.Forms.TextBox();
+            this.txtMes = new System.Windows.Forms.TextBox();
+            this.txtAnio = new System.Windows.Forms.TextBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblDia
+            // 
+            this.lblDia.AutoSize = true;
+            this.lblDia.Location = new System.Drawing.Point(48, 59);
+            this.lblDia.Name = "lblDia";
+            this.lblDia.Size = new System.Drawing.Size(28, 16);
+            this.lblDia.TabIndex = 0;
+            this.lblDia.Text = "Día";
+            // 
+            // lblMes
+            // 
+            this.lblMes.AutoSize = true;
+            this.lblMes.Location = new System.Drawing.Point(48, 112);
+            this.lblMes.Name = "lblMes";
+            this.lblMes.Size = new System.Drawing.Size(33, 16);
+            this.lblMes.TabIndex = 1;
+            this.lblMes.Text = "Mes";
+            // 
+            // lblAño
+            // 
+            this.lblAño.AutoSize = true;
+            this.lblAño.Location = new System.Drawing.Point(48, 163);
+            this.lblAño.Name = "lblAño";
+            this.lblAño.Size = new System.Drawing.Size(31, 16);
+            this.lblAño.TabIndex = 2;
+            this.lblAño.Text = "Año";
+            // 
+            // txtDia
+            // 
+            this.txtDia.Location = new System.Drawing.Point(122, 59);
+            this.txtDia.Name = "txtDia";
+            this.txtDia.Size = new System.Drawing.Size(100, 22);
+            this.txtDia.TabIndex = 3;
+            // 
+            // txtMes
+            // 
+            this.txtMes.Location = new System.Drawing.Point(122, 112);
+            this.txtMes.Name = "txtMes";
+            this.txtMes.Size = new System.Drawing.Size(100, 22);
+            this.txtMes.TabIndex = 4;
+            // 
+            // txtAnio
+            // 
+            this.txtAnio.Location = new System.Drawing.Point(122, 163);
+            this.txtAnio.Name = "txtAnio";
+            this.txtAnio.Size = new System.Drawing.Size(100, 22);
+            this.txtAnio.TabIndex = 5;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(51, 217);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(75, 23);
+            this.btnEnviar.TabIndex = 6;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(213, 217);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 7;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Location = new System.Drawing.Point(298, 59);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(0, 16);
+            this.lblFecha.TabIndex = 8;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(132, 217);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 9;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 268);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.txtAnio);
+            this.Controls.Add(this.txtMes);
+            this.Controls.Add(this.txtDia);
+            this.Controls.Add(this.lblAño);
+            this.Controls.Add(this.lblMes);
+            this.Controls.Add(this.lblDia);
+            this.Name = "Form1";
+            this.Text = "Práctica de controles";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDia;
+        private System.Windows.Forms.Label lblMes;
+        private System.Windows.Forms.Label lblAño;
+        private System.Windows.Forms.TextBox txtDia;
+        private System.Windows.Forms.TextBox txtMes;
+        private System.Windows.Forms.TextBox txtAnio;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.Button btnLimpiar;
+    }
+}
+
---
+++ Pract1-Controles Elaborado por Diego Sanjur/Pract1-Controles Elaborado por Diego Sanjur/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pract1_Controles_Elaborado_por_Diego_Sanjur
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            string[] meses = { "enero", "febrero", "marzo", "abril", "mayo", "junio",
+                       "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre" };
+
+            string mes = txtMes.Text.Trim().ToLower();
+            int indiceMes = Array.IndexOf(meses, mes);
+
+            if (indiceMes == -1)
+            {
+                MessageBox.Show("Ingrese un mes válido");
+                txtMes.Focus();
+                return;
+            }
+
+            int numeroMes = indiceMes + 1;
+
+            if (!int.TryParse(txtDia.Text.Trim(), out int dia))
+            {
+                MessageBox.Show("Ingrese un número válido para el día");
+                txtDia.Focus();
+                return;
+            }
+
+            if (dia < 1 || dia > 31)
+            {
+                MessageBox.Show("El día debe estar entre 1 y 31");
+                txtDia.Focus();
+                return;
+            }
+
+            int anio = DateTime.Now.Year;
+            int diasDelMes = DateTime.DaysInMonth(anio, numeroMes);
+
+            if (dia > diasDelMes)
+            {
+                MessageBox.Show($"{txtMes.Text.Trim()} de {anio} tiene {diasDelMes} días");
+                txtDia.Focus();
+                return;
+            }
+
+            string mesCapitalizado = char.ToUpper(mes[0]) + mes.Substring(1);
+            lblFecha.Text = $"La fecha es: {dia} de {mesCapitalizado} de {anio}";
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtDia.Text = "";
+            txtMes.Text = "";
+            txtAnio.Text = "";
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Cerrando programa");
+            this.Close();
+        }
+    }
+}
